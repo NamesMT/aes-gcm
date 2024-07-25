fed27c2d23686123c7992197061372bd6c638b24
Trung Dang <dangquoctrung123@gmail.com>
2024-07-25 18:31:32 +0700

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+
+import sharedConfig from './shared.config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude!, 'shared.config.ts'],
+    },
+  },
+  resolve: {
+    alias: sharedConfig.alias,
+  },
+  ...sharedConfig,
+})
